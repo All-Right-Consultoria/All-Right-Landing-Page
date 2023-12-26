b9027792548a8a6ffb146588b65c1d561ff2c4ee
Victor W. Hostert <victorhostert@gmail.com>
2023-12-26 16:29:12 -0300

    included new model and migration

--- allright/blog/models.py
+++ allright/blog/models.py
@@ -1,10 +1,24 @@
+from datetime import datetime
 from django.db import models
 
 class Post(models.Model):
     titulo = models.CharField(
         max_length=255,
-        verbose_name='Título'
+        verbose_name='Título',
     )
     conteudo = models.TextField(
-        verbose_name='Conteúdo'
+        verbose_name='Conteúdo',
     )
+    midia = models.ImageField(
+        verbose_name='Mídia',
+        upload_to='media/',
+        null=True,
+        blank=True,
+    )
+    data_publicacao = models.DateTimeField(
+        verbose_name='Data de publicação',
+        auto_now_add=True
+    )
+
+class Case(Post):
+    pass
