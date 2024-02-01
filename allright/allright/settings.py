51436c6462b70bbf8b03c4b69e006e599a3e6d4a
pedrovh <contact.pedrovh@gmail.com>
2024-01-31 21:37:15 -0300

    Ajusta projeto para funcionar em prod

--- allright/allright/settings.py
+++ allright/allright/settings.py
@@ -123,10 +123,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'assets/static/',
-)
-
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
