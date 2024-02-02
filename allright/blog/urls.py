8d6cd6b7f4913d33be9d1ffe79ac2deb8a74abc3
Victor W. Hostert <victorhostert@gmail.com>
2024-02-02 00:24:45 -0300

    otimização mobile

--- allright/blog/urls.py
+++ allright/blog/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import LandingPageView, AboutPageView, CaseListView
+from .views import LandingPageView, CaseListView
 
 
 urlpatterns = [
     path('', LandingPageView),
-    path('about/', AboutPageView),
     path('cases/', CaseListView.as_view(), name='cases-list'),
     # path('posts/', PostListView), # TODO: A ser implementado futuramente
 ]
