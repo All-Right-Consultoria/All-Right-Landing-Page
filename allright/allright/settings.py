7a8528b1070dfa59fadfd79bc7114514ec86c232
Victor W. Hostert <victorhostert@gmail.com>
2024-03-18 14:11:31 -0300

    changed static settings

--- allright/allright/settings.py
+++ allright/allright/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'assets/'
+STATIC_ROOT = BASE_DIR / 'assets/static/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'assets/static/',
+    BASE_DIR / 'assets/',
 )
 
 MEDIA_URL = 'media/'
